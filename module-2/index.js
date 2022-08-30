ca66c125719a75ec68bb55ba399e1825f680cdf0
SabbirSami <smd71430@gmail.com>
2022-08-30 12:27:00 +0600

    send response json file

--- module-2/index.js
+++ module-2/index.js
@@ -7,6 +7,10 @@ const server = http.createServer((req, res) => {
     if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write("<p>This is home page</p>");
+        // res.end();
+    } else if (req.url === "/course") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ course: "ACC" }));
         res.end();
     }
 });
