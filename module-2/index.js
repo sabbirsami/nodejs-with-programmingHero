be8be648662a7dfc61ac2dde81e139af93c88f3b
SabbirSami <smd71430@gmail.com>
2022-08-30 12:36:42 +0600

    Exploring URL Module

--- module-2/index.js
+++ module-2/index.js
@@ -2,17 +2,24 @@ const { name, add } = require("./other");
 // console.log(name);
 
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<p>This is home page</p>");
-        // res.end();
-    } else if (req.url === "/course") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.write(JSON.stringify({ course: "ACC" }));
-        res.end();
-    }
+    // if (req.url === "/") {
+    //     res.writeHead(200, { "Content-Type": "text/html" });
+    //     res.write("<p>This is home page</p>");
+    //     // res.end();
+    // } else if (req.url === "/course") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     res.write(JSON.stringify({ course: "ACC" }));
+    //     res.end();
+    // }
+
+    const url_address =
+        "http://localhost:5000/contact?name=sami&country=bangladesh";
+    const parsed_url = url.parse(url_address, true);
+    const queryObject = parsed_url.query;
+    console.log(queryObject);
 });
 
 const PORT = 5000;
