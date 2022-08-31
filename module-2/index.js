68919ebbe913aa6f360f2a60d498133978894c46
SabbirSami <smd71430@gmail.com>
2022-08-31 10:24:43 +0600

    Explore file system

--- module-2/index.js
+++ module-2/index.js
@@ -3,23 +3,47 @@ const { name, add } = require("./other");
 
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
+// const server = http.createServer((req, res) => {
+//     // if (req.url === "/") {
+//     //     res.writeHead(200, { "Content-Type": "text/html" });
+//     //     res.write("<p>This is home page</p>");
+//     //     // res.end();
+//     // } else if (req.url === "/course") {
+//     //     res.writeHead(200, { "Content-Type": "application/json" });
+//     //     res.write(JSON.stringify({ course: "ACC" }));
+//     //     res.end();
+//     // }
+
+//     const url_address =
+//         "http://localhost:5000/contact?name=sami&country=bangladesh";
+//     const parsed_url = url.parse(url_address, true);
+//     const queryObject = parsed_url.query;
+//     console.log(queryObject);
+// });
 const server = http.createServer((req, res) => {
-    // if (req.url === "/") {
-    //     res.writeHead(200, { "Content-Type": "text/html" });
-    //     res.write("<p>This is home page</p>");
-    //     // res.end();
-    // } else if (req.url === "/course") {
-    //     res.writeHead(200, { "Content-Type": "application/json" });
-    //     res.write(JSON.stringify({ course: "ACC" }));
-    //     res.end();
-    // }
+    if (req.url === "/") {
+        // fs.readFile("data.txt", (err, data) => {
+        //     if (err) {
+        //         res.write("Failed to read data");
+        //         res.end();
+        //     } else {
+        //         res.write(data);
+        //         res.end();
+        //     }
+        // });
 
-    const url_address =
-        "http://localhost:5000/contact?name=sami&country=bangladesh";
-    const parsed_url = url.parse(url_address, true);
-    const queryObject = parsed_url.query;
-    console.log(queryObject);
+        fs.writeFile("data.txt", "Hello Node Js", (err) => {
+            if (err) {
+                res.write("Failed to write data");
+                res.end();
+            } else {
+                res.write("data write successfully");
+                res.end();
+            }
+        });
+    }
 });
 
 const PORT = 5000;
